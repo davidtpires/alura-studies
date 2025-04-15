97af0b0667fbc94e9be435fa018b74a0141e3c48
David T. <davidt.info@gmail.com>
2025-04-14 21:03:27 -0300

    Mostrando a tarefa selecionada

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,22 +1,32 @@
-import { ITarefa } from '../../../types/tarefa';
-import style from '../Lista.module.scss'
+import { ITarefa } from "../../../types/tarefa";
+import style from "../Lista.module.scss";
 
-interface Props extends ITarefa{
-    selecionaTarefa: (tarefaSelecionada: ITarefa) => void 
+interface Props extends ITarefa {
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void;
 }
 
-export default function Item({ tarefa, tempo, selecionado, completado, id, selecionaTarefa }: Props) {
-  console.log('item atual: ', { tarefa, tempo, selecionado, completado, id });
+export default function Item({
+  tarefa,
+  tempo,
+  selecionado,
+  completado,
+  id,
+  selecionaTarefa,
+}: Props) {
+  console.log("item atual: ", { tarefa, tempo, selecionado, completado, id });
 
   return (
-    <li 
-      className={style.item} onClick={() => selecionaTarefa({
-        tarefa,
-        tempo,
-        selecionado,
-        completado,
-        id
-      })}
+    <li
+      className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`}
+      onClick={() =>
+        selecionaTarefa({
+          tarefa,
+          tempo,
+          selecionado,
+          completado,
+          id,
+        })
+      }
     >
       <h3> {tarefa} </h3>
       <span>{tempo}</span>
